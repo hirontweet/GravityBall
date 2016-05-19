deafd1331178fb67424c598f34281f66c709b828
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-05-20 02:15:54 +0900

    Added TouchEvent and related method for shooting off the Object
    TouchEvent added to the class to detect the user's touch on the screen.
    Inside the onTouchEvent callback, there's shootDown() method that calculates the distance of the object and the AimField instance and if it's in the range, the object will be null.

--- app/src/main/java/jp/spaism/gravityball/GravityBallView.java
+++ app/src/main/java/jp/spaism/gravityball/GravityBallView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -25,6 +27,21 @@ public class GravityBallView extends SurfaceView implements SurfaceHolder.Callba
         super(context);
 
         getHolder().addCallback(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.v(getClass().toString(), "X: " + touchX + ", Y: " + touchY);
+
+            shootDown();
+        }
+
+        return true;
     }
 
     public void drawView(Canvas canvas){
@@ -114,9 +131,55 @@ public class GravityBallView extends SurfaceView implements SurfaceHolder.Callba
         if(!circle300.isInScreen()){
             circle300 = null;
         }
-
-
     }
+
+    public void shootDown(){
+        float aimRadius = aimField.getRadius();
+        float aimX = aimField.getX();
+        float aimY = aimField.getY();
+
+        if (circle100.isInScreen()) {
+            // Try catching the exception of NullPointerException because the ball might be out of the screen.
+            try {
+                if (aimX - aimRadius <= circle100.getX() &&
+                        circle100.getX() <= aimX + aimRadius &&
+                            aimY - aimRadius <= circle100.getY() &&
+                                circle100.getY() <= aimY + aimRadius) {
+                    circle100 = null;
+                }
+            } catch (NullPointerException e) {
+
+            }
+        }
+
+        if (circle200.isInScreen()) {
+            try {
+                if (aimX - aimRadius <= circle200.getX() &&
+                        circle200.getX() <= aimX + aimRadius &&
+                        aimY - aimRadius <= circle200.getY() &&
+                        circle200.getY() <= aimY + aimRadius) {
+                    circle200 = null;
+                }
+            } catch (NullPointerException e) {
+
+            }
+        }
+
+        if (circle300.isInScreen()) {
+            try {
+                if (aimX - aimRadius <= circle300.getX() &&
+                        circle300.getX() <= aimX + aimRadius &&
+                        aimY - aimRadius <= circle300.getY() &&
+                        circle300.getY() <= aimY + aimRadius) {
+                    circle300 = null;
+                }
+            } catch (NullPointerException e) {
+
+            }
+        }
+    }
+
+
 
     private class DrawThread extends Thread{
         private boolean isFinished = false;
