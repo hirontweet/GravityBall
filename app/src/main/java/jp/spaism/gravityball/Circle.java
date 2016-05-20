330b1586d5f47496393042a1e0b28ebe242d462a
Hirofumi NAKANO <jasdfhnakano@gmail.com>
2016-05-21 02:00:23 +0900

    (CircleBugFix)Circle.isInScreen refactoring had problem. Now it's been solved
    The problem was the statement had to be negated but instead it was returning true.

--- app/src/main/java/jp/spaism/gravityball/Circle.java
+++ app/src/main/java/jp/spaism/gravityball/Circle.java
@@ -55,7 +55,7 @@ public class Circle {
 
     public boolean isInScreen(){
 
-        return (mPoint_X - mRadius > mTargetCanvasWidth) || (mPoint_Y - mRadius > mTargetCanvasHeight) || (mPoint_X + mRadius < 0) || (mPoint_Y + mRadius < 0);
+        return !((mPoint_X - mRadius > mTargetCanvasWidth) || (mPoint_Y - mRadius > mTargetCanvasHeight) || (mPoint_X + mRadius < 0) || (mPoint_Y + mRadius < 0));
 
     }
 
